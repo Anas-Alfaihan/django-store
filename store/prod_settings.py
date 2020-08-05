71dd6b226eca83ffccfefb759c571086923d9a4c
Anas Alfaihan <narotoaozomake99@gmail.com>
2020-08-04 19:07:08 -0700

    adding DEBUG option and email service

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -4,7 +4,11 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+debug_option = os.environ.get('DEBUG').lower()
+if debug_option == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -13,3 +17,11 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = 'smtp-relay.sendinblue.com'
+EMAIL_HOST_USER = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = False
