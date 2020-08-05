145747c5ec9247db1c181e5d8e11175fcbd274a7
Anas Alfaihan <narotoaozomake99@gmail.com>
2020-08-04 19:12:17 -0700

    .

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -24,4 +24,4 @@ EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_HOST_USER = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
