a761b0198976e12109cd0347d69fa1145cee4693
Anas Alfaihan <narotoaozomake99@gmail.com>
2020-08-04 21:59:57 -0700

    .

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -20,7 +20,7 @@ DATABASES = {
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+#DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = 'smtp-relay.sendinblue.com'
 EMAIL_HOST_USER = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
