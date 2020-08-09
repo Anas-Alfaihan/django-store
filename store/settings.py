ea742c9571025b5cc8e10c9ec37b004638c3db6a
Anas Alfaihan <narotoaozomake99@gmail.com>
2020-08-09 04:09:39 -0700

    .

--- store/settings.py
+++ store/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 ALLOWED_HOSTS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 MEDIA_URL = '/media/'
